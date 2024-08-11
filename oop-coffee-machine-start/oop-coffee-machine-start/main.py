223eb32d453fe7a72c12a3cab2fb86f72b60daba
Shivam <Shivamkumar188132@gmail.com>
2024-08-11 23:48:09 +0530

    modified coffe_checker

--- oop-coffee-machine-start/oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/oop-coffee-machine-start/main.py
@@ -4,9 +4,22 @@ from money_machine import MoneyMachine
 
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
-# menu = Menu()
-# menu_item = MenuItem()
+menu = Menu()
+menu_item = MenuItem()
 
+# print(menu.get_items())
+# coffee_maker.is_resource_sufficient("espresso")
+# money_machine.report()
+choice = input("what do you need? \nlatte | espresso | cappuccino\n")
+is_on = True
+while is_on:
+  if choice == "report":
+      coffee_maker.report()
+      is_on = False
+  elif choice == "latte" or "espresso" or "cappuccino":
+      drink = menu.find_drink(choice)
+      coffee_maker.is_resource_sufficient(drink)
 
-coffee_maker.report()
-money_machine.report()
+  else:
+      break
+
