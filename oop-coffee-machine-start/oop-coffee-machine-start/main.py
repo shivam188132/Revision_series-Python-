0cc99699e5b59af1726c66de2d013493d3295414
Shivam <Shivamkumar188132@gmail.com>
2024-08-11 12:44:40 +0530

    added prettytable_practise.py for practise

--- oop-coffee-machine-start/oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/oop-coffee-machine-start/main.py
@@ -2,3 +2,11 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+# menu = Menu()
+# menu_item = MenuItem()
+
+
+coffee_maker.report()
+money_machine.report()
